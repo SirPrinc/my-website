d724cbe9fa2d9e8148097dfe71b9d857c1094c18
Prince Mfinanga <noahprince2@icloud.com>
2024-12-18 11:39:29 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch("navbar.html")
+    .then(response => {
+        if(!response.ok) {
+            throw new Error("Failed to load navbar!");
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById('navbar').innerHTML = data;
+    })
+    .catch(error => {
+        console.error("Error loading the navbar!");
+    });
+    });
